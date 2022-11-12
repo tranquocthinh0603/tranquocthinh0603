1ce7c1539bb2a8ca4ab016a4cc74f859e91b36ec
Tran Quoc Thinh <109783774+tranquocthinh0603@users.noreply.github.com>
2022-11-12 18:02:03 +0700

    Update dom-qwaszzcdfdsaadsfff014752555455.js

--- dom-qwaszzcdfdsaadsfff014752555455.js
+++ dom-qwaszzcdfdsaadsfff014752555455.js
@@ -1,7 +1,3 @@
-// thẻ meta
 var meta = '<p>Test thử</p>';
-//hàm gọi các javascript---->
 var js = '<p>Test thử</p>';
-// hàm gọi phần head --->
-var head = meta+js;
 document.getElementById("col").innerHTML=meat;

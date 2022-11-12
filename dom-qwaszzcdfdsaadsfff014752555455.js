477bdddb3c7d0c4f62806b78e5aef8b30dce195a
Tran Quoc Thinh <109783774+tranquocthinh0603@users.noreply.github.com>
2022-11-12 19:52:51 +0700

    Update dom-qwaszzcdfdsaadsfff014752555455.js

--- dom-qwaszzcdfdsaadsfff014752555455.js
+++ dom-qwaszzcdfdsaadsfff014752555455.js
@@ -1,4 +1,5 @@
 
 var meta = '<meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>';
+var link = '<form id="senmail" name="formte" method="POST"><input type="email" id="email-li" placeholder="Địa chỉ email"><input type="textarea" name="nd" id="nd" value="Nội dung cần gửi"><button type="button" onclick="sendEmail()">Gửi email</button></form>'
 
-document.getElementById("col").innerHTML = meta;
+document.getElementById("col").innerHTML = meta+link;

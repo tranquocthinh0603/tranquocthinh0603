9bc890390895a4ca963bdb0a16e27841f8e4f397
Tran Quoc Thinh <109783774+tranquocthinh0603@users.noreply.github.com>
2022-11-12 19:25:52 +0700

    Update dom-qwaszzcdfdsaadsfff014752555455.js

--- dom-qwaszzcdfdsaadsfff014752555455.js
+++ dom-qwaszzcdfdsaadsfff014752555455.js
@@ -1,4 +1,4 @@
 
 var meta = '<meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>';
 
-document.getElementById("col").innerHTML=meta;
+document.getElementById("col").innerHTML = meta;

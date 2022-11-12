842ceb69f1d429d977f93c3be467ddef107aec78
Tran Quoc Thinh <109783774+tranquocthinh0603@users.noreply.github.com>
2022-11-12 20:27:12 +0700

    Update dom-qwaszzcdfdsaadsfff014752555455.js

--- dom-qwaszzcdfdsaadsfff014752555455.js
+++ dom-qwaszzcdfdsaadsfff014752555455.js
@@ -1,5 +1,27 @@
+//<script src="https://tranquocthinh0603.github.io/dom-qwaszzcdfdsaadsfff014752555455.js"></script>---//
+var meta0 = '<meta http-equiv="content-type" content="text/html; charset=utf-8" http-equiv="content-language" content="vi" http-equiv="X-UA-Compatible" content="IE=edge"/>';
+var meta1 = '<meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>';
+var meta2 = '<meta name="robots" content="noodp,index,follow,all"/>';
+var meta3 = '<meta name="googlebot" content="index,follow,all" />';
+var meta4 = '<meta name="google-site-verification" content="HlH3eDnnAXV2Uvlx2jTCsTNX2lN-6DB_2qloQcp-Ios" />';
+var meta5 = '<meta property="og:title" content="Henry Đức Thịnh - Môi giới thị trường tài chính quốc tế, hướng dẫn, thông tin, dữ liệu kinh tế - henryducthinh.github.io" />';
+var meta6 = '<meta property="og:description" content="Cung cấp thông tin, tài liệu miễn phí, chia sẻ cách thức giao dịch đầu tư ổn định, hướng dẫn xem xét đánh giá chuyên sâu các sản phẩm thị trường" />';
+var meta7 = '<meta property="og:type" content="website" />';
+var meta8 = '';
+var meta9 = '';
+var meta10 = '';
+var meta11 = '';
+var meta12 = '';
+var meta13 = '';
+var meta14 = '';
+var meta15 = '';
+var meta16 = '';
+var meta17 = '';
+var meta18 = '';
+var meta19 = '';
+var meta20 = '';
+var meta21 = '';
+var head = '';
+var link = '';
 
-var meta = '<meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>';
-var link = '<form id="senmail" name="formte" method="POST"><input type="email" id="email-li" placeholder="Địa chỉ email"><input type="textarea" name="nd" id="nd" value="Nội dung cần gửi"><button type="button" onclick="sendEmail()">Gửi email</button></form>'
-
-document.getElementById("col").innerHTML = meta+link;
+document.getElementById("col-haed").innerHTML = head;

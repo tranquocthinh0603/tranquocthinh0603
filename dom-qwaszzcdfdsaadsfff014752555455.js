dac4d7929624786c58e09eb9ddbb142b71872603
Tran Quoc Thinh <109783774+tranquocthinh0603@users.noreply.github.com>
2022-11-12 17:02:18 +0700

    Create dom-qwaszzcdfdsaadsfff014752555455.js

---
+++ dom-qwaszzcdfdsaadsfff014752555455.js
@@ -0,0 +1,13 @@
+// thẻ meta
+var meta = '<p>Test thử</p>';
+//hàm gọi các javascript---->
+var js = '<p>Test thử</p>';
+// hàm gọi phần head --->
+var head = meta+js;
+document.getElementById("head-col").innerHTML=meta;
+// hàm gọi các body --->
+var body = '';
+document.getElementById("body-col").innerHTML=body;
+// hàm gọi các footer ----->
+var footer = '';
+document.getElementById("footer-col").innerHTML=footer;
